cd6ed4e8bc17133de189f23597cbf04ceb7db64e
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-31 13:59:58 -0300

    feat: adiciona FotoController

---
+++ app/Http/Controllers/Manager/FotoController.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use App\Models\Foto;
+use App\Services\ImagemService;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+class FotoController extends Controller
+{
+    protected $imagemService;
+
+    public function __construct(ImagemService $imagemService)
+    {
+        $this->imagemService = $imagemService;
+    }
+
+    public function getFotos($idGaleria)
+    {
+        try {
+            $foto = Foto::where([
+                'galeria_id' => $idGaleria,
+                'excluido' => NULL,
+            ])
+                ->orderBy('ordem', 'ASC')
+                ->get();
+
+            if ($foto->isEmpty()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Fotos não encontradas'
+                ], 404);
+            }
+
+            return response()->json([
+                'fotos' => $foto->map(function ($item) {
+                    return [
+                        'id' => $item->id,
+                        'imagem' => $item->imagem,
+                        'ordem' => $item->ordem,
+                        'visivel' => $item->visivel,
+                        'galeria_id' => $item->galeria_id
+                    ];
+                })
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao buscar fotos',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function getFoto($idFoto)
+    {
+        try {
+            $foto = Foto::where([
+                'id' => $idFoto,
+                'excluido' => NULL,
+            ])->first();
+
+            if (!$foto) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Foto não encontrada'
+                ], 404);
+            }
+
+            return response()->json([
+                'id' => $foto->id,
+                'imagem' => $foto->imagem,
+                'ordem' => $foto->ordem,
+                'visivel' => $foto->visivel,
+                'galeria_id' => $foto->galeria_id
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao buscar foto',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function createFoto(Request $request, $idGaleria)
+    {
+        $this->validate($request, [
+            'ordem' => 'nullable',
+            'visivel' =>  'nullable|boolean',
+            'imagem' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ], [
+            'imagem.required' => 'Por favor, insira uma imagem.',
+            'imagem.image' => 'O arquivo deve ser uma imagem!',
+            'imagem.mimes' => 'A imagem deve ser jpeg, png ou jpg!',
+            'imagem.max' => 'A imagem não pode ter mais de 2MB!',
+        ]);
+
+        $dados = $request->only(['ordem', 'visivel', 'imagem']);
+
+        try {
+            $response = $this->imagemService->cadastrarFoto($dados, $idGaleria);
+
+            return response()->json([
+                'success' => true,
+                'data' => $response,
+                'message' => 'Foto criada com sucesso.'
+            ], 201);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao criar foto',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function updateFoto(Request $request, $idFoto)
+    {
+        $this->validate($request, [
+            'ordem' => 'nullable',
+            'visivel' =>  'nullable|boolean',
+            'imagem' => 'image|mimes:jpeg,png,jpg|max:2048',
+        ], [
+            'imagem.image' => 'O arquivo deve ser uma imagem!',
+            'imagem.mimes' => 'A imagem deve ser jpeg, png ou jpg!',
+            'imagem.max' => 'A imagem não pode ter mais de 2MB!',
+        ]);
+
+        $dados = $request->only(['ordem', 'visivel', 'imagem']);
+
+        try {
+            $response = $this->imagemService->atualizarFoto($dados, $idFoto);
+
+            return response()->json([
+                'success' => true,
+                'data' => $response,
+                'message' => 'Foto atualizada com sucesso.'
+            ], 200);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao criar foto',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function orderFoto(Request $request)
+    {
+        $this->validate($request, [
+            '*.id' => 'required|integer',
+            '*.ordem' => 'required|integer',
+        ], [
+            'ordem.required' => 'Por favor, informe a ordem da foto.',
+            'ordem.integer' => 'A ordem da imagem é um valor inválido!'
+        ]);
+
+        $dados = $request->all();
+
+        try {
+            $response = $this->imagemService->atualizarOrdem($dados);
+
+            return response()->json([
+                'success' => true,
+                'data' => $response,
+            ], 200);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao alterar a ordem das fotos.',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function visibleFoto(Request $request, $idFoto)
+    {
+        $this->validate($request, [
+            'visivel' => 'required|boolean',
+        ], [
+            'visivel.required' => 'Por favor, informe a visibilidade da foto.',
+            'visivel.boolean' => 'A visibilidade da foto é um valor inválido!'
+        ]);
+
+        $dados = $request->only(['visivel']);
+
+        try {
+            $response = $this->imagemService->atualizarVisibilidade($dados, $idFoto);
+
+            return response()->json([
+                'success' => true,
+                'data' => $response,
+                'message' => 'Visibilidade alterada com sucesso.'
+            ], 200);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao alterar a visibilidade da foto.',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function deleteFoto($idFoto)
+    {
+        try {
+            $response = $this->imagemService->excluirFoto($idFoto);
+
+            return response()->json([
+                'success' => true,
+                'data' => $response,
+                'message' => 'Foto excluida com sucesso.'
+            ], 200);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao excluir foto.',
+                'error' => $e->getMessage(),
+            ], 500);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro inesperado ao processar a solicitação.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+}
