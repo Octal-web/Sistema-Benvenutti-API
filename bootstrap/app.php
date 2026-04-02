7a78436e3b244ff4767c1e403f933dc0d682844e
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-02 10:36:30 -0300

    feat: adiciona carregamento do config services

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -63,6 +63,7 @@ $app->singleton(
 
 $app->configure('app');
 $app->configure('mail');
+$app->configure('services');
 
 /*
 |--------------------------------------------------------------------------
@@ -144,7 +145,7 @@ $app->register(\Illuminate\Mail\MailServiceProvider::class);
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 class_alias(Tymon\JWTAuth\Facades\JWTAuth::class, 'JWTAuth');
